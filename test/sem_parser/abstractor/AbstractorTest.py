1d7c16f4c87b9d739678b43722700917af1e5179
David Schwenke <schwenkedavid@t-online.de>
2022-02-06 16:25:51 +0100

    Reworked tests as experiments from presentation examples.

--- test/sem_parser/abstractor/AbstractorTest.py
+++ test/sem_parser/abstractor/AbstractorTest.py
@@ -1,33 +1,48 @@
 import unittest
 
-from smart_ccg.sem_parser.entity_abstractor.Abstractor import Abstractor
+import pandas as pd
+
+from smart_ccg.sem_parser.abstractor.Abstractor import Abstractor
+from smart_ccg.sem_parser.abstractor.Table import Table
 
 
 class AbstractorTest(unittest.TestCase):
-    def setUp(cls) -> None:
-        cls.abstractor = Abstractor()
+    def setUp(self) -> None:
+        self.abstractor = Abstractor()
 
     def test_abstractor_initialization(self):
         parse, = self.abstractor.dependency_parser.raw_parse("This is a test.")
         self.assertEqual("This\tDT\t4\tnsubj\nis\tVBZ\t4\tcop\na\tDT\t4\tdet\ntest\tNN\t0\tROOT\n.\t.\t4\tpunct\n",
-                          parse.to_conll(4))
+                         parse.to_conll(4))
 
-    def test_abstractor_sentenece_with_case(self):
-        sentence, subsentences, lifted_string = self.abstractor.lifted_sentence_instances(
-            "Sort them by age ascending."
-        )
-        self.assertEqual("Sort <obj> <case>.", lifted_string)
+    def test_abstractor_no_table_single_sentence_object_list(self):
+        string = "Create the table students containing the columns name, surname, mark and id."
+        sentence, subsentences = self.abstractor.extract_sentence_instances_from(string)
 
-    def test_abstractor_sentence_with_object_list(self):
-        sentence, subsentences, lifted_string = self.abstractor.lifted_sentence_instances(
-            "Show me the colors, age and range of all the vehicles."
-        )
-        self.assertEqual("Show me <objs>[4, 6, 8] <case> <obj>.", lifted_string)
+        self.assertEqual(0, len(sentence.values))
+        self.assertEqual(["students", "name", "surname", "mark", "id"], [obj.word for obj in sentence.objects])
+        self.assertEqual(0, len(sentence.cases))
+        self.assertEqual("Create [table] [,column]", sentence.lifted())
+        self.assertEqual(0, len(subsentences))
 
-    def test_abstractor_lifted_composed_sentence(self):
-        sentence, subsentences, lifted_string = self.abstractor.lifted_sentence_instances(
-            "Show me the colors, age and range of the vehicles with a range larger than 300 miles and an age of less than 2 years and sort them by age ascending.")
-        self.assertEqual("Show me <objs>[4, 6, 8] <case> <obj> and sort <obj> <case>.", lifted_string)
+    def test_abstractor_no_table_single_sentence_object_list_ungrammatical(self):
+        string = "Create the table students containing the columns name surname mark and id."
+        sentence, subsentences = self.abstractor.extract_sentence_instances_from(string)
+
+        self.assertEqual(0, len(subsentences))
+        self.assertNotEqual("Create [table] [,column]", sentence.lifted())
+
+    def test_abstractor_table_single_sentence_condition(self):
+        string = "Show all students which have passed the exam."
+        table = Table(["exam", "id"], "students", pd.DataFrame())
+        sentence, subsentences = self.abstractor.extract_sentence_instances_from(string)
+
+        self.assertEqual(0, len(subsentences))
+        self.assertEqual(["passed"], [value.word for value in sentence.values])
+        self.assertEqual(["students", "exam"], [obj.word for obj in sentence.objects])
+        self.assertEqual(["have"], [case.word for case in sentence.cases])
+        self.assertEqual("Show [table] [case]", sentence.lifted(table))
+        self.assertEqual(["have [value] [column]"], sentence.case_lifted(table))
 
 
 if __name__ == '__main__':
