40bf6f6a6ae5e6afdce441cd9ea910511bdd4e88
David Schwenke <schwenkedavid@t-online.de>
2022-08-29 03:29:52 +0200

    Removed combinator.

--- smart_ccg/combinator/util/CombinatorStorage.py
+++
@@ -1,7 +0,0 @@
-class CombinatorStorage:
-    def __init__(self, storage_root):
-        self.storage_root = storage_root
-        self.dataset_path = storage_root / "dataset"
-
-    def save_dataset(self, examples):
-        pass
