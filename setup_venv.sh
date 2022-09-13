09c5be0bd11458d80357d951a6d0563283ea2f9c
David Schwenke <schwenkedavid@t-online.de>
2022-09-13 23:54:59 +0200

    Updated setup and requirements.

---
+++ setup_venv.sh
@@ -0,0 +1,6 @@
+python3 -m venv ./venv
+source ./venv/bin/activate
+pip install --upgrade pip
+pip install -r requirements.txt
+pip install depccg
+cp ./res/setup/lstm_parser_elmo.tar.gz ./venv/lib/python3.10/site-packages/depccg/models/
