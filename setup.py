f4a8ac372269b039c65b6d652816f23f45a48834
David Schwenke <schwenkedavid@t-online.de>
2022-09-14 13:04:12 +0200

    Updated project setup and CI workflow.

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(
+    name="EfficientParsing",
+    description="Efficient semantic parser for spreadsheet applications",
+    author="David Schwenke",
+    package_dir={"": "efficient_parsing"},
+    packages=["src"],
+    version='1.0'
+)
--- setup_venv.sh
+++ setup_venv.sh
@@ -4,3 +4,4 @@ pip install --upgrade pip
 pip install -r requirements.txt
 pip install depccg
 cp ./res/setup/lstm_parser_elmo.tar.gz ./venv/lib/python3.10/site-packages/depccg/models/
+pip install -e .
